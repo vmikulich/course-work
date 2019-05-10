e63f6e9565e3d9096d0451d49cb6473c20ab00d5
VitalyMikulich <mr.wital1999@gmail.com>
2019-05-10 11:06:24 +0300

    change url

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb+srv://Vlad:12345@cluster0-m1ird.mongodb.net/test?retryWrites=true',
+    mongoURI: 'mongodb+srv://Vlad:nashproject1.@coursework-k9oqd.mongodb.net/test?retryWrites=true',
     jwt: 'dev-jwt'
 }
